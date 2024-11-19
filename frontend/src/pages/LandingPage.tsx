6892d0380035cd0305a198c7861aa285f565323a
Une Umoh <une.umoh@pau.edu.ng>
2024-11-19 12:31:48 +0100

    Landing Page

---
+++ frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div className="flex flex-1 flex-col">
+      <div></div>
+    </div>
+  );
+};
+
+export default LandingPage;
