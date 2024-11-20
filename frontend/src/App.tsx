cf23141a41a5f2237a8031fd38a2d6ab2265aea8
DavidOmoera <omoeradavid@gmail.com>
2024-11-20 17:08:03 +0100

    upds

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
+
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";  
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Define routes for LandingPage and Home */}
+        {/* Define routes for LandingPage, SignUpPage, and Home */}
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} /> {/* Add the SignUp route */}
       </Routes>
     </BrowserRouter>
   );
