66c7a181be15a7912b4f4a38c7cf8cbad4e60b4c
Une Umoh <une.umoh@pau.edu.ng>
2024-11-27 14:26:01 +0100

    minor changes to user

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,5 +1,9 @@
 const createUser = (req, res) => {
   const { username, password } = req.body();
+  async function hashPassword(message) {
+    var hashedPassword = sha256(message);
+    return hashPassword;
+  }
   const hashedPassword = hashPassword(password);
 
   res.send("User Created Successfully");
--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,6 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
-async function hashPassword(message) {
-  var hashedPassword = sha256(message);
-  return hashPassword;
-}
-
 app.use(express.json());
 
 app.get("/", (req, res) => [res.send("Hello World")]);
